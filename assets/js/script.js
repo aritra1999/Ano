f4a6c3d656575154021ac2fc20dad6e9ebcc433d
Hrishav Kumar <jha2506@gmail.com>
2019-12-04 01:32:57 +0530

    New Design

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    const copyText = document.getElementById("myurl");
-    copyText.select();
-  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-  document.execCommand("copy");
-}
